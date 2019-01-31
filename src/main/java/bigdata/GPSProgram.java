a84664df24e7cbde831f01a485bb3c6f6495d46d
Apodeus <ordonez.romain@gmail.com>
2019-01-31 13:29:19 +0100

    Yay !

--- src/main/java/bigdata/GPSProgram.java
+++ src/main/java/bigdata/GPSProgram.java
@@ -33,35 +33,46 @@ public class GPSProgram {
 
         JavaRDD<String> gpsFile = ctx.textFile("/user/raw_data/simple-gps-points-120312.txt");
 
+        JavaRDD<String> rddDistinct = gpsFile.map(line -> {
+            String[] tok = line.split(",");
+            long lat = Long.parseLong(tok[0]);
+            long longitude = Long.parseLong(tok[1]);
+
+            long lat2 = lat / 1000;
+            long long2 = longitude / 1000;
+
+            return lat2 + "," + long2;
+        }).distinct();
+
         //Ici le but est de determiner dans quelle tuile est le point
         // Ainsi que de determiner, en pourcentage, le décalage depuis le coin supérieur gauche de la tuile
         // pour trouver le bon pixel plus tard.
         // Le shift n'est pas le meme selon si on est Nord/Sud ou Ouest/Est
-        JavaPairRDD<String, Value> rdd1 = gpsFile.mapToPair(line -> {
+        JavaPairRDD<String, Value> rdd1 = rddDistinct.mapToPair(line -> {
             String[] coords = line.split(",");
             long ns = Long.parseLong(coords[0]);
             long we = Long.parseLong(coords[1]);
 
-            long nsKey = ns / 10000000L; // 10^7
-            long weKey = we / 10000000L;
+            long nsKey = ns / 10000L; // 10^7
+            long weKey = we / 10000L;
 
             //Rethink this when equal 0 ....
             float shiftX;
             float shiftY;
             if (nsKey < 0) { // if south
                 nsKey = 90 + Math.abs(nsKey);
-                shiftY = ((ns / 10000000f) % 1);
+                shiftY = ((ns / 10000f) % 1);
             } else {
                 nsKey = 90 - nsKey;
-                shiftY = 1 - ((Math.abs(ns) / 10000000f) % 1);
+                shiftY = 1 - ((Math.abs(ns) / 10000f) % 1);
             }
 
             if (weKey < 0) {  //if west
                 weKey = 180 - Math.abs(weKey);
-                shiftX = 1 - ((Math.abs(we) / 10000000f) % 1);
+                shiftX = 1 - ((Math.abs(we) / 10000f) % 1);
             } else {
                 weKey = 180 + weKey;
-                shiftX = (we / 10000000f) % 1;
+                shiftX = (we / 10000f) % 1;
             }
             Coord k = new Coord((int)weKey, (int)nsKey, 0);
             String key = (int)weKey + "-" + (int)nsKey + "-" + 0;
@@ -73,9 +84,6 @@ public class GPSProgram {
         JavaPairRDD<String, Tuple2<Integer, Integer>> rdd2 = rdd1.mapValues(v -> {
             int posX = (int) (v.shiftX * (TILE_SIZE - 1));
             int posY = (int) (v.shiftY * (TILE_SIZE - 1));
-            if(posX < 0 || posY < 0){
-                System.out.println("negative postions : "+ posX + ";" + posY);
-            }
             return new Tuple2<>(posX, posY);
         });
 
@@ -86,23 +94,23 @@ public class GPSProgram {
 
         // On crée la tuile du point pour simplifier le traitement (j'avais la flemme de penser autrement
         // puis ca sera plus simple pour plus tard
-        JavaPairRDD<String, int[]> rdd4 = rdd3.mapValues(it -> {
-            int[] tile = new int[TILE_SIZE * TILE_SIZE];
-            Arrays.fill(tile, 0);
+        JavaPairRDD<String, boolean[]> rdd4 = rdd3.mapValues(it -> {
+            boolean[] tile = new boolean[TILE_SIZE * TILE_SIZE];
+            Arrays.fill(tile, false);
             it.forEach(tuple -> {
                 int x = tuple._1;
                 int y = tuple._2;
                 int index = x + y * TILE_SIZE;
-                tile[index] = tile[index] + 1;
+                tile[index] = true;
             });
             return tile;
-        }).persist(StorageLevel.DISK_ONLY());
+        }).cache();
 
         JavaPairRDD<String, byte[]> rdd5 = rdd4.mapValues(tile -> {
             int length = tile.length;
-            ByteBuffer bb = ByteBuffer.allocate(length * Integer.BYTES);
+            ByteBuffer bb = ByteBuffer.allocate(length *  Byte.BYTES);
             for (int i = 0; i < length; ++i) {
-                bb.putInt(tile[i]);
+                bb.put(tile[i] ? (byte)1 : (byte) 0);
             }
             return bb.array();
         });
@@ -111,12 +119,13 @@ public class GPSProgram {
 
         hbaseDAO.bulkSaveRDD(compressRdd5, STORAGE_HBASE_GPS + "0", TILES, GPS);
 
-        for(int nbAggr = 0; nbAggr < 8; ++nbAggr){
+        int aggr = 8;
+        for(int nbAggr = 0; nbAggr < aggr; ++nbAggr){
 
             // On commence les aggrégations des résultats précédents, il faut peut etre mettre en cache le rdd4 pour l'envoyer en
             // BDD, similaire au groupeByKey, il faut regarder ce qu'il faut quand on utilise des objets customs pour que ca fonctionne
             // equals ? compareTo ?...
-            JavaPairRDD<String, Iterable<Tuple2<String, int[]>>> rdd5Aggr = rdd4.groupBy(t -> {
+            JavaPairRDD<String, Iterable<Tuple2<String, boolean[]>>> rdd5Aggr = rdd4.groupBy(t -> {
                 String[] coords = t._1.split("-");
                 int newX = Integer.valueOf(coords[0]) / 2;
                 int newY = Integer.valueOf(coords[1]) / 2;
@@ -126,9 +135,9 @@ public class GPSProgram {
 
             // Ici le but est de composer la grande tuile
             // Et de recopier chaque mini tuile dans la grande tuile dans le bon coin.
-            JavaPairRDD<String, int[]> rdd6Aggr = rdd5Aggr.mapValues(it -> {
-                int[] newTile = new int[TILE_SIZE * TILE_SIZE * 4];
-                Arrays.fill(newTile, 0);
+            JavaPairRDD<String, boolean[]> rdd6Aggr = rdd5Aggr.mapValues(it -> {
+                boolean[] bigTile = new boolean[TILE_SIZE * TILE_SIZE * 4];
+                Arrays.fill(bigTile, false);
                 it.forEach(tuple -> {
                     String[] coords = tuple._1.split("-");
                     int coordX = Integer.parseInt(coords[0]);
@@ -137,41 +146,41 @@ public class GPSProgram {
                     int shiftX = TILE_SIZE * (coordX % 2);
                     int shiftY = TILE_SIZE * (coordY % 2);
 
-                    int[] tile = tuple._2;
+                    boolean[] tile = tuple._2;
                     for (int y = 0; y < TILE_SIZE; ++y) {
                         for (int x = 0; x < TILE_SIZE; ++x) {
                             int indexBigTile = (x + shiftX) + ((y + shiftY) * TILE_SIZE * 2);
                             int currIndex = x + y * TILE_SIZE;
-                            newTile[indexBigTile] += tile[currIndex];
+                            bigTile[indexBigTile] = tile[currIndex];
                         }
                     }
                 });
-                return newTile;
+                return bigTile;
             });
 
             // On va maintenant aggréger les valeurs de la grande tuile vers la tuile de taille normale.
             // Avoir la somme par pixel va permettre de jouer sur les nuances de gris pour plus tard.
-            JavaPairRDD<String, int[]> rdd7aggr = rdd6Aggr.mapValues(bigTile -> {
-                int[] finalTile = new int[TILE_SIZE * TILE_SIZE];
+            JavaPairRDD<String, boolean[]> rdd7aggr = rdd6Aggr.mapValues(bigTile -> {
+                boolean[] finalTile = new boolean[TILE_SIZE * TILE_SIZE];
                 for (int y = 0; y < TILE_SIZE; ++y) {
                     for (int x = 0; x < TILE_SIZE; ++x) {
-                        int sum = 0;
+                        boolean isPresent = false;
                         for (int j = y*2; j <= y*2 + 1; ++j) {
                             for (int i = x*2; i <= x*2 + 1; ++i) {
-                                sum += bigTile[i + j * TILE_SIZE * 2];
+                                isPresent = isPresent || bigTile[i + j * TILE_SIZE * 2];
                             }
                         }
-                        finalTile[x + y * TILE_SIZE] = sum;
+                        finalTile[x + y * TILE_SIZE] = isPresent;
                     }
                 }
                 return finalTile;
-            }).cache();
+            }).persist(StorageLevel.DISK_ONLY());
 
             JavaPairRDD<String, byte[]> rdd8 = rdd7aggr.mapValues(tile -> {
                 int length = tile.length;
-                ByteBuffer bb = ByteBuffer.allocate(length * Integer.BYTES);
+                ByteBuffer bb = ByteBuffer.allocate(length * Byte.BYTES);
                 for (int ind = 0; ind < length; ++ind) {
-                    bb.putInt(tile[ind]);
+                    bb.put(tile[ind] ? (byte)1 : (byte) 0);
                 }
                 return bb.array();
             });
@@ -183,6 +192,5 @@ public class GPSProgram {
             rdd4 = rdd7aggr;
         }
         rdd4.unpersist();
-
     }
 }
